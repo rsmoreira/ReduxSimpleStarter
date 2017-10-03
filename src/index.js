6feab24598d72a8fd7342c96372cba00689c71c9
rsmoreira <rsmoreira.mail@gmail.com>
2017-10-02 22:22:59 -0300

    Adjusting code according with Section 1

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// Create a new Component.  
+// This Component should produce some HTML
+// https://jsx.github.io/
+const App = () => {
+    return <div>Hi!</div>
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Take this Component generated HTML and put it on the page (in the DOM)]
+
+ReactDOM.render(<App />, document.querySelector('.container'));
