c0b9491c259b24070e6414f8a5bc5395ba0fce0e
Rafael Severo Moreira <rmoreira@ecrmac-100839.local>
2017-10-03 09:08:44 -0300

    Adding youtube api features - credentials + dependency

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// https://console.developers.google.com/apis/credentials
+// npm install --save youtube-api-search
+const API_KEY = 'AIzaSyD61-V3Wx6uu89eHCxDm-3-dCRXlhFQvD';
+
 // Create a new Component.  
 // This Component should produce some HTML
 // https://jsx.github.io/
