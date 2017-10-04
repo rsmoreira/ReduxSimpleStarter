d71aa0434ecda0d5a7076f1b3b386f3e65481dc0
rsmoreira <rsmoreira.mail@gmail.com>
2017-10-03 21:57:19 -0300

    Adding files from 14 lecture

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearcBar from './components/search_bar';
+
 // https://console.developers.google.com/apis/credentials
 // npm install --save youtube-api-search
 const API_KEY = 'AIzaSyD61-V3Wx6uu89eHCxDm-3-dCRXlhFQvD';
@@ -9,7 +11,11 @@ const API_KEY = 'AIzaSyD61-V3Wx6uu89eHCxDm-3-dCRXlhFQvD';
 // This Component should produce some HTML
 // https://jsx.github.io/
 const App = () => {
-    return <div>Hi!</div>
+    return (
+        <div>
+            <SearcBar />
+        </div>
+    );
 }
 
 
