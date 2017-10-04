ffa2c9c2e840aefcc5f16b69e6754b7c25e0ba26
Rafael Severo Moreira <rmoreira@ecrmac-100839.local>
2017-10-04 09:30:13 -0300

    Lecture 15 - updating search_bar component from a Function Component to a Class Based Component extending React.Component.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react'; // ', { Component }' - it avoids you writing 'extends React.Component', so you just need to write 'extends Component'
 
+// ES6 - React Class Based component
 
-const SearchBar = () => {
-    return <input/>
+class SearchBar extends Component {
+    render() {
+        return <input />;
+
+    }
 }
+
 
 export default SearchBar;
